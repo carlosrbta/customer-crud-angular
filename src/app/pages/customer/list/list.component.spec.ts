a419e07790d80fe31c0393f09c9d2aea46378159
Carlos Roberto Augusto Junior <carlosrbta@gmail.com>
2020-08-11 12:01:19 -0300

    Unit tests fixes

--- src/app/pages/customer/list/list.component.spec.ts
+++ src/app/pages/customer/list/list.component.spec.ts
@@ -1,8 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 import { PhonePipe } from '../../../pipes/phone.pipe';
 import { CpfPipe } from '../../../pipes/cpf.pipe';
 import { TruncatePipe } from '../../../pipes/truncate.pipe';
-
 import { CustomerListComponent } from './list.component';
 
 describe('CustomerListComponent', () => {
@@ -12,6 +15,11 @@ describe('CustomerListComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CustomerListComponent, TruncatePipe, CpfPipe, PhonePipe],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FontAwesomeTestingModule,
+      ],
     }).compileComponents();
   }));
 
